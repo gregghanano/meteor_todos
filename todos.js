42be8860e8a72de638f6c7baeaf5878dc92a0758
Gregg Hanano <gregg.hanano@gmail.com>
2015-10-11 22:00:58 -0700

    built a very simple todo list

--- todos.js
+++ todos.js
@@ -1,6 +1,39 @@
 Todos = new Mongo.Collection('todos');
 if(Meteor.isClient){
     // client code goes here
+    //---- template Todos helpers ------
+    Template.todos.helpers({
+      'todo': function(){
+        return Todos.find({}, {sort: {createdAt: -1}});
+      }
+    });
+    //-----end------
+    //----- template addTodos events ------
+    Template.addTodo.events({
+      'submit form':function(e){
+        e.preventDefault();
+        var todoName = $('[name="todoName"]').val();
+        Todos.insert({
+          name: todoName,
+          completed: false,
+          createdAt: new Date()
+        });
+        $('[name="todoName"]').val('');
+      }
+    });
+    //----- end ------
+    //------ template todoItem events ------
+    Template.todoItem.events({
+      'click .delete-todo':function(e){
+        e.preventDefault();
+        var documentId = this._id;
+        var confirm = window.confirm("Delete this task?");
+        if(confirm){
+          Todos.remove({ _id: documentId });
+        }
+      }
+    });
+    //------ end -------
 }
 
 if(Meteor.isServer){
